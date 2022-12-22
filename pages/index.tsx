15b15bfe4eec3771c3f8fa4e7ce1a5225735b06a
Euler <mombergereuler@gmail.com>
2022-12-21 23:07:40 -0300

    feat: Autenticação com SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { FormEvent, useContext, useState } from 'react'
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import { FormEvent, useContext, useState } from 'react';
+import type { NextPage } from 'next';
+
+import styles from '../styles/Home.module.css';
 import { AuthContext } from '../context/AuthContext';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
@@ -29,4 +31,10 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
